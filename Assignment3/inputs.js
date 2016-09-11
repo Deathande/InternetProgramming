d7aa1dbeeb27eef668035b8db2a942116c28be16
Dock O'Neal <Deathande@Docks-MacBook-Pro.local>
2016-09-11 17:32:58 -0400

    Can now remove elements and add them back without loosing thier information

--- Assignment3/inputs.js
+++ Assignment3/inputs.js
@@ -1,33 +1,56 @@
+var inputContainers = [];
+var removed = [];
+
 function add() {
-  document.getElementById('container').appendChild(generateDiv());
+  if (removed.length > 0) {
+    console.log(';test');
+    var newDiv = removed.pop();
+  }
+  else {
+    var newDiv = generateDiv();
+  }
+  inputContainers.push(newDiv);
+  document.getElementById('container').appendChild(newDiv);
 }
 
 function subtract(element) {
+  var index = inputContainers.indexOf(element);
+  console.log(index);
+  removed.push(element);
+  inputContainers.splice(index, 1);
+  console.log(inputContainers);
   element.remove();
+}
+
+function setNumber(element) {
+  var len = element.childNodes[0].value.length;
+  element.childNodes[2].innerHTML = len;
 }
 
 function generateInput() {
   var input = document.createElement('input');
-  //var att = document.createAttribute('type');
-  //att.type = 'text';
   input.setAttribute('type', 'text');
-  //input.setAttributeNode(att);
+  input.setAttribute('onkeyup', 'setNumber(this.parentElement)');
   return input;
 }
 
 function generateButton() {
   var button = document.createElement('button');
-  //var att = document.createAttribute('onClick');
   button.appendChild(document.createTextNode('-'));
   button.setAttribute('onClick', 'subtract(this.parentElement)');
-  //att.onClick = 'subtract()';
-  //button.setAttributeNode(att);
   return button;
+}
+
+function generateLabel() {
+  var label = document.createElement('label');
+  label.innerHTML = '0';
+  return label;
 }
 
 function generateDiv() {
   var div = document.createElement('div');
   div.appendChild(generateInput());
   div.appendChild(generateButton());
+  div.appendChild(generateLabel());
   return div;
 }
